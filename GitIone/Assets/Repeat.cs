8e0cf5025aa874d93b87be4e771b8f93945e7540
phy5prt <philip.r.tate@gmail.com>
2018-10-06 13:00:05 +0100

    block tumble simulator

---
+++ GitIone/Assets/Repeat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repeat : MonoBehaviour {
+
+Vector3 thisTransform;
+float timesReset = 0;
+
+	// Use this for initialization
+	void Start () {
+		thisTransform=gameObject.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	if(Time.realtimeSinceStartup-timesReset*3>3f)
+	{gameObject.transform.position=thisTransform;
+
+	timesReset++;
+
+
+	}
+
+
+				
+	}
+}
